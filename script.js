76ed98a4258a4e8d7e4e88257305726062ea41d2
Gianluigi Trontini <39003797+gianluigitrontini@users.noreply.github.com>
2020-01-23 19:55:50 +0100

    Updated logic
    Can't select less than 5 minutes of work or break. Created Start button. Logic still unfinished.

--- script.js
+++ script.js
@@ -7,24 +7,50 @@ $(document).ready(function () {
     var sessionCount = parseInt($('#sessionTime').html());
     var buzzer = $('#buzzer')[0];
 
-    $('#playPause').hide();
-    $('#reset').hide();
+    $('#playPause, #reset').hide();
 
+    $('#start').click(function () {
+        var counter = setInterval(timer, 1000);
+
+        function timer() {
+            $('#playPause') == true;
+            $('#start').html(sessionCount);
+            $('#playPause, #reset').show();
+            sessionCount -= 1;
+
+            if (sessionCount === 0) {
+                buzzer.play();
+                clearInterval(counter);
+            }
+            if ($('#playPause').click && $('#playPause') == true) {
+                clearInterval(timer);
+                $('#playPause') == false;
+            } else if ($('#playPause').click && $('#playPause') == false) {
+                setInterval(timer, 1000);
+                $('#playPause') == true;
+            }
+        }
+
+    });
     $('#subSession').click(function () {
-        sessionCount -= 5;
-        $('#sessionTime').html(sessionCount)
-    })
+        if (sessionCount > 5) {
+            sessionCount -= 5;
+            $('#sessionTime').html(sessionCount)
+        }
+    });
     $('#subBreak').click(function () {
-        breakCount -= 5;
-        $('#breakTime').html(breakCount)
-    })
+        if (breakCount > 5) {
+            breakCount -= 5;
+            $('#breakTime').html(breakCount)
+        }
+    });
     $('#addSession').click(function () {
         sessionCount += 5;
         $('#sessionTime').html(sessionCount)
-    })
+    });
     $('#addBreak').click(function () {
         breakCount += 5;
         $('#breakTime').html(breakCount)
-    })
+    });
 
 });
