fa3864c030e9ba96e16cef4259158195ba6de406
Gianluigi Trontini <39003797+gianluigitrontini@users.noreply.github.com>
2020-01-23 18:37:49 +0100

    Upload sound and start writing logic for session and break time counters

--- script.js
+++ script.js
@@ -2,4 +2,29 @@ $(document).ready(function () {
     $('#myNavBtn').on('click', function () {
         $('.animated-icon3').toggleClass('open');
     });
+
+    var breakCount = parseInt($('#breakTime').html());
+    var sessionCount = parseInt($('#sessionTime').html());
+    var buzzer = $('#buzzer')[0];
+
+    $('#playPause').hide();
+    $('#reset').hide();
+
+    $('#subSession').click(function () {
+        sessionCount -= 5;
+        $('#sessionTime').html(sessionCount)
+    })
+    $('#subBreak').click(function () {
+        breakCount -= 5;
+        $('#breakTime').html(breakCount)
+    })
+    $('#addSession').click(function () {
+        sessionCount += 5;
+        $('#sessionTime').html(sessionCount)
+    })
+    $('#addBreak').click(function () {
+        breakCount += 5;
+        $('#breakTime').html(breakCount)
+    })
+
 });
