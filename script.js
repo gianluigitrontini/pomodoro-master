73ccd26ef8a7482149691b843e4d24daebae1ba3
Gianluigi Trontini <39003797+gianluigitrontini@users.noreply.github.com>
2020-01-23 20:01:50 +0100

    Update script.js timer

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ $(document).ready(function () {
 
         function timer() {
             $('#playPause') == true;
+
             $('#start').html(sessionCount);
             $('#playPause, #reset').show();
             sessionCount -= 1;
